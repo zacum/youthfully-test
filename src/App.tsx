fcfae1313547b8c8c8d40296f0293b747540febb
zacum <fisaque0104@gmail.com>
2023-01-23 14:43:29 +0900

    init: create react app with yarn typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import { IImage } from './pages/Home';
+import {
+  Routes, Route
+} from 'react-router-dom';
+import Loading from './components/Loading';
+
+function App() {
+  const [detail, setDetail] = useState<IImage>();
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
